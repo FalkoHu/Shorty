82d91f9df8d2c48afaeba33aa0f0dda2839da850
Falko Hussain <falko.hussain@gmail.com>
2020-07-21 21:58:12 -0500

    added service class with createshortURL method

--- src/main/java/com/shorty/urlshorter/service/UrlService.java
+++ src/main/java/com/shorty/urlshorter/service/UrlService.java
@@ -1,14 +1,24 @@
 package com.shorty.urlshorter.service;
 
 import com.shorty.urlshorter.model.UrlShortenerRequest;
+import com.shorty.urlshorter.repository.MongoDdRepository;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class UrlService {
 
-    public String createNewShortUrl(UrlShortenerRequest urlShortenerRequest) {
-        return "Hello";
+    private MongoDdRepository mongoDdRepository;
+
+    @Autowired
+    public UrlService(MongoDdRepository mongoDdRepository) {
+        this.mongoDdRepository = mongoDdRepository;
+    }
+
+    public UrlShortenerRequest createNewShortUrl(UrlShortenerRequest urlShortenerRequest){
+        log.debug("createNewShortUrl called with : {}", urlShortenerRequest);
+        return mongoDdRepository.save(urlShortenerRequest);
     }
 }
